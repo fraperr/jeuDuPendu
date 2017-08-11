85dfc99501121853445b50098ebbbcc0658de1eb
François <fraperr@gmail.com>
2017-08-11 16:42:24 +0200

    Dans le fichier pendu.py testé si le fichier scores existe et si le nom du joueur est présent

--- pendu.py
+++ pendu.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import pickle
+import pickle # pour les fichiers en mode binaire
 
 nom = input("Veuillez saisir votre nom: ")
 
+# vérifier si le fichier des scores existe, si ce n'est pas le cas
+# créer un dictionaire gérant le score
 try:
     with open('scores', 'rb') as fichier:
         monDepickler = pickle.Unpickler(fichier)
-        scoreRecupere = monDepickler.load()
-except FileNotFoundError:
-   fichier = open('scores', 'w')
-   fichier.close()
+        score = monDepickler.load()
 
+except FileNotFoundError:
+    score = {}
+
+# vérifier si le joueur existe, sinon le créer avec un score de 0
+# et l'enregistrer dans le fichier
+try:
+    score[nom]
+except KeyError:
+    score[nom] = 0
+    with open("scores", "wb") as fichier:
+        monPickler = pickle.Pickler(fichier)
+        monPickler.dump(score)
+
+# récupérer le fichier du score et l'affecter au dictionaire score.
+with open("scores", "rb") as fichier:
+    monDepickler = pickle.Unpickler(fichier)
+    score = monDepickler.load()
+
+print(score)
