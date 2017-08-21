c2b6eeff54c7690362e66731ce7832cbd5293788
François <fraperr@gmail.com>
2017-08-21 12:32:53 +0200

    ajouté à pendu.py la possibilité de remplacer une lettre partout dans le mot, et à donnees.py un module qui remplace par des étoiles les lettres d'un mot et un autre qui trouve toutes les positions d'une lettre dans un mot

--- donnees.py
+++ donnees.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import pickle # pour les fichiers en mode binaire
+
+def insereDesEtoiles(mot):
+    motCache = ''
+    for lettre in mot:
+        motCache += '*'
+    return motCache
+
 def dico():
     mots = ["bâteau", "chat", "pendule", "soleil", "lune", "malade", "citron",
             "boisson", "prière", "vinaigre", "vitesse", "oignon", "bière",
@@ -21,5 +29,35 @@ def saisirCaract(message):
         if saisie.isalpha():
             estVide = False
         else:
-            print("Votre nom ne doit comporter que des lettres!")
+            print("Vous ne pouvez saisir que des lettres.")
     return saisie
+
+# vérifier si le fichier des scores existe, si ce n'est pas le cas
+# la fonction renvoie une variable dictionnaire contenant les scores des joueurs
+def fichierExiste(monFichier):
+    try:
+        with open(monFichier, 'rb') as fichier:
+            monDepickler = pickle.Unpickler(fichier)
+            score = monDepickler.load()
+
+    except FileNotFoundError:
+        score = {}
+
+    return score
+
+def trouveToutesLesPositions(motL, lettre):
+    i = 0
+    positionL = []
+    while i < len(motL):
+        # On vérifie si la lettre choisit est dans le mot.
+        try:
+            position =  motL.index(lettre, i)
+        except ValueError:
+            i = len(motL)
+        # Dans ce cas on remplace l'étoile par la lettre et on affiche
+        # la nouvelle chaine ainsi formée.
+        else:
+            positionL.append(position)
+            i = position + 1
+    return positionL
+
--- pendu.py
+++ pendu.py
@@ -4,19 +4,13 @@
 import pickle # pour les fichiers en mode binaire
 import donnees
 import random
+import pdb
 
 # on vérifier que le joueur entre au moins un caractère.
-messageNom = "Veuillez saisir votre nom: "
-nom =  donnees.saisirCaract(messageNom)
-# vérifier si le fichier des scores existe, si ce n'est pas le cas
-# créer un dictionaire gérant le score
-try:
-    with open('scores', 'rb') as fichier:
-        monDepickler = pickle.Unpickler(fichier)
-        score = monDepickler.load()
+nom =  donnees.saisirCaract("Veuillez saisir votre nom: ")
 
-except FileNotFoundError:
-    score = {}
+# Si le fichier existe on récupère le score dans la variable score
+score = donnees.fichierExiste("scores")
 
 # vérifier si le joueur existe, sinon le créer avec un score de 0
 # et l'enregistrer dans le fichier
@@ -24,42 +18,45 @@ try:
     score[nom]
 except KeyError:
     score[nom] = 0
-    with open("scores", "wb") as fichier:
-        monPickler = pickle.Pickler(fichier)
-        monPickler.dump(score)
 
-# récupérer le fichier du score et l'affecter au dictionaire score.
-with open("scores", "rb") as fichier:
-    monDepickler = pickle.Unpickler(fichier)
-    score = monDepickler.load()
+with open("scores", "wb") as fichier:
+    monPickler = pickle.Pickler(fichier)
+    monPickler.dump(score)
 
 # on récupère la liste des mots
 listeDesMots = donnees.dico()
 nombreDeMots = len(donnees.dico()) # calcul du nombre de mots
 choixDuMot = random.randrange(nombreDeMots) # on en choisit un au hasard
-print(nombreDeMots)
 unMot = listeDesMots[choixDuMot]
 
-motCache = ''
-for lettre in unMot:
-    motCache += '*'
+# Affichichage du mot cachée sous forme '*' et creation d'un liste contenant
+# la même chose
+motCache = donnees.insereDesEtoiles(unMot)
 motCacheL = list(motCache)
 print(motCache)
 
 i = 0
+# pdb.set_trace()
 while i < 8:
-    choixLettre = input("Choisissez une lettre: ")
+    # Le joueur peut choisir une lettre
+    choixLettre = ''
+    while len(choixLettre) != 1:
+        choixLettre = donnees.saisirCaract("Choisissez une lettre: ")
+        if len(choixLettre) > 1:
+            print("Vous n'avez droit qu'à une seule lettre.")
+
+    # On convertir la chaine de caractère mot en dictionaire.
     unMotL = list(unMot)
-    try:
-        position =  unMotL.index(choixLettre)
-    except ValueError:
-        print("Cette lettre n'est pas dans le mot")
-    else:
+    listeDesPositions = donnees.trouveToutesLesPositions(unMotL, choixLettre)
+
+    # On remplce les étoiles par la lettre trouvée.
+    for position in listeDesPositions:
         del motCacheL[position]
         motCacheL.insert(position, choixLettre)
+
     motCache = ''
     for lettre in  motCacheL:
         motCache += lettre
+    print(motCache)
     i += 1
 
-print(motCache)
