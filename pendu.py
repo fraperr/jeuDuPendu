10c3ffb3d097f87460c294838869baa08ded586f
fraperr <fraperr@gmail.com>
2017-09-17 19:07:23 +0200

    Créé fonction.py pour regrouper les fonctions. Ajouté à pendu.py l'enregistrement du score dans un fichier + commentaires

--- pendu.py
+++ pendu.py
@@ -3,18 +3,17 @@
 
 import pickle # pour les fichiers en mode binaire
 import donnees # liste des modules personnels
+import fonction
 import random # permet de générer un nombre aléatoire
 # import pdb # debug
 
 # on vérifier que le joueur entre au moins un caractère.
-nom =  donnees.saisirCaract("Veuillez saisir votre nom: ")
+nom =  fonction.saisirCaract("Veuillez saisir votre nom: ")
 
 # Si le fichier existe on récupère le score dans la variable score
-# sinon un crée un fichier de score vide
-score = donnees.fichierExiste("scores")
+score = fonction.fichierExiste("scores")
 
 # vérifier si le joueur existe, sinon le créer avec un score de 0
-# et l'enregistrer dans le fichier
 try:
 	score[nom]
 except KeyError:
@@ -22,9 +21,6 @@ except KeyError:
 
 # affichage du score
 print("Votre score actuel: "  + str(score[nom]))
-with open("scores", "wb") as fichier:
-	monPickler = pickle.Pickler(fichier)
-	monPickler.dump(score)
 
 # on récupère la liste des mots
 listeDesMots = donnees.dico()
@@ -34,45 +30,51 @@ unMot = listeDesMots[choixDuMot]
 
 # Affichichage du mot cachée sous forme '*' et creation d'un liste contenant
 # la même chose
-motCache = donnees.insereDesEtoiles(unMot)
+motCache = fonction.insereDesEtoiles(unMot)
 motCacheL = list(motCache)
 
-i = 0
+i = 0 # initialisation du compteur
 # pdb.set_trace()
-reponse = ''
-while i < 8 and reponse.lower() != unMot:
+reponse = '' # initialisation de reponse
+nbreDeTours = donnees.nombreChances()
+while i < nbreDeTours and reponse.lower() != unMot: # on a droit a 8 chances
 	# Le joueur peut choisir une lettre
 	choixLettre = ''
-	while len(choixLettre) != 1:
-		choixLettre = donnees.saisirCaract("Choisissez une lettre: ").lower()
+	while len(choixLettre) != 1: # Une seule lettre peut-être saisie
+		choixLettre = fonction.saisirCaract("Choisissez une lettre: ").lower()
 		if len(choixLettre) > 1:
 			print("Vous n'avez droit qu'à une seule lettre.")
 
 	# On convertir la chaine de caractère mot en dictionaire.
 	unMotL = list(unMot)
-	listeDesPositions = donnees.trouveToutesLesPositions(unMotL, choixLettre)
+	# Indice de toutes les occurences possibles
+	listeDesPositions = fonction.trouveToutesLesPositions(unMotL, choixLettre)
 
 	# On remplce les étoiles par la lettre trouvée.
 	for position in listeDesPositions:
 		del motCacheL[position]
 		motCacheL.insert(position, choixLettre)
 
-	print(donnees.listEnChaine(motCacheL))
-	i += 1
+	# Affichage du résultat
+	print(fonction.listEnChaine(motCacheL))
+	i += 1 # Tour suivant
 
+	# L'utilisateur a la possiblité de trouver le mot
 	ouiNon = ''
 	while ouiNon != "o".lower() and ouiNon != "n".lower():
 		ouiNon = input("Voulez-vous donner la solution: ")
 
 	if ouiNon == "o":
 		reponse = input("Introduisez votre mot: ")
-
+# En cas de réussite on affiche un message et on ajuste les points
 if reponse.lower() == unMot:
 	print("Bravo vous avez gagné!")
 	points = 8 - i
 	score[nom] += points
+	fonction.enregistre(score, "scores")
 else:
 	print("Désolé, vous avez perdu!")
 
+# Affichage du score final
 print("Votre score est de : ", score[nom])
 
